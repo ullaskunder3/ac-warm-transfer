1d1fcb990e27e07375534fedbd3a1f260261fc6b
ullaskunder3 <ullaskunder3@gmail.com>
2025-09-18 19:38:12 +0530

    initial fastapi

---
+++ backend/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from app.api.routes import call, token
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Register routers
+app.include_router(token.router, prefix="/api")
+app.include_router(call.router, prefix="/api")
